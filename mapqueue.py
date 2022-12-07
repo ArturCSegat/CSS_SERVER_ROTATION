e7f3f08b2a51f6354a23a0c945cca35dc3f3a557
Artur Camera Segat <arturcamerasegat@gmail.com>
2022-12-07 00:57:15 -0300

    Refactorinf class and changing so it cahnges te name of the current file

--- mapqueue.py
+++ mapqueue.py
@@ -7,54 +7,75 @@ def random_map(folder): #global for __init__ usage
     map = random.choice(os.listdir(folder))
     return map
 
+def clear_dir(folder):
+
+    for f in os.listdir(folder):
+        os.remove(f"./{folder}/{f}")
+
 
 class Queue:
 
     def __init__(self, folder):
 
+
+
         self.folder = folder
         
         origin = folder # redundant lol??
 
-        self.current_name = random_map(origin)
+        current_name = random_map(origin)
 
-        self.next_name = self.current_name
+        next_name = current_name
 
-        while(self.next_name == self.current_name): # so current and next are always different
-            self.next_name = random_map(origin)
+        while(next_name == current_name): # so current and next are always different
+            next_name = random_map(origin)
 
-        shutil.copy(f"{origin}/{self.current_name}", './server_queue/current.txt') #so names are constanst just change the content
-        shutil.copy(f"{origin}/{self.next_name}", './server_queue/next.txt')
+        shutil.copy(f"{origin}/{current_name}", f"./server_queue/{current_name}") #so names are constanst just change the content
+        shutil.copy(f"{origin}/{next_name}",f"./server_queue/{next_name}")
         
 
-        self.current_map = './server_queue/current.txt' #just path
-        self.next_map = './server_queue/next.txt'
+        self.current_map = f"./server_queue/{current_name}" #just path
+        self.next_map = f"./server_queue/{next_name}"
 
     
     def switchMaps(self):
 
-        previous = self.current_name # saving the previously played map so you dont play it twice
-        
+        previous = self.current_map # saving the previously played map so you dont play it twice
 
         shutil.copy(self.next_map, self.current_map) #switches content
+        
+        os.remove(self.current_map)
+
+        self.current_map = self.next_map # switches state in class
+
+
+        next_path = self.current_map
+
+        while(f"./server_queue/{next_path}" == f"./server_queue/{self.current_map}" or f"./server_queue/{next_path}" == f"./server_queue/{previous}"): # fds gambiarra doida
+            next_path = random_map(self.folder)
+        
+        self.next_map = f"./server_queue/{next_path}"
+        shutil.copy(f"./{self.folder}/{next_path}", self.next_map)
 
 
 
-        self.current_name = self.next_name # switches name
 
-        while (self.next_name == self.current_name or self.next_name == previous): # so current and next are always different
-            self.next_name = random_map(self.folder)
-            shutil.copy(f"{self.folder}/{self.next_name}", self.next_map)
+
+
+       
+
+                    
+            
         
 
     def showQueue(self):
 
-        print(f"The current map is {self.current_name} and the next one is {self.next_name}")
+        print(f"The current map is {self.current_map} and the next one is {self.next_map}")
 
 
  # random debug code
 
-""" queue = Queue('.\maps') 
+queue = Queue('.\maps') 
 
 queue.showQueue()
 
@@ -62,7 +83,7 @@ time.sleep(5)
 
 queue.switchMaps()
 
-queue.showQueue() """
+queue.showQueue()
 
 
 
