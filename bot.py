327c27a0b9b99f794ff4c0f238730bdbb1f4251d
Artur Camera Segat <arturcamerasegat@gmail.com>
2022-12-09 17:59:19 -0300

    Tentativa inicial de bot de discord, construcor, destructor e skip command

--- bot.py
+++ bot.py
@@ -1,1 +1,32 @@
-# bot integration goes here someday
+import discord
+from discord.ext import commands
+from server import Server
+
+
+bot = commands.Bot(command_prefix=":", intents=None)
+
+server = Server(10, 15, "C://steamcmd/css_ds")
+
+@bot.event
+async def on_ready():
+    server.startServer()
+    channel = await bot.get_channel(581159663345860619) # ggez
+    await channel.send(f"Server ta on no mapa {server.queue.current_map.name}")
+
+@bot.event
+async def on_resumed():
+    server.killServer()
+    channel = await bot.get_channel(581159663345860619) # ggez
+    await channel.send(f"matei o server")
+
+
+@bot.command
+async def skip(ctx):
+    server.nextMap()
+    await ctx.send(f"Mapa mudado para {server.queue.current_map.name} entra dnv")
+
+
+
+
+bot.run("MTA1MDg3MjAzMTg0OTIxODIyOQ.GIRYd9.uwySGDswru96eV4v6VZG7IgtSRkE228Czs_lQ0")
+
